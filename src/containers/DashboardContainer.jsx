5c5a4b13ac78e1ccd624ddb713f159de39047429
Emmanuel Morales <emorales0024@gmail.com>
2016-02-27 15:53:03 -0600

    Add right menu for increase/decrease options (non functional)

--- src/containers/DashboardContainer.jsx
+++ src/containers/DashboardContainer.jsx
@@ -6,6 +6,12 @@ import '../scss/dashboard.scss';
 import Card from 'material-ui/lib/card/card';
 import CardHeader from 'material-ui/lib/card/card-header';
 import Avatar from 'material-ui/lib/avatar';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import Divider from 'material-ui/lib/divider';
+import FontIcon from 'material-ui/lib/font-icon';
 
 const DashboardContainer = ({ dashboard }) => {
   const goals = Object.getOwnPropertyNames(dashboard);
@@ -19,7 +25,19 @@ const DashboardContainer = ({ dashboard }) => {
             title={ <span className="dashboard-widget-title">{ dashboard[goalKey].title }</span> }
             subtitle={ <span className="dashboard-widget-subtitle">{ dashboard[goalKey].value + ` / ` + dashboard[goalKey].limit }</span> }
             avatar={ <Avatar size={ 60 } src={ dashboard[goalKey].avatar } /> }
-          />
+          >
+          <IconMenu
+            className="dashboard-widget-iconmenu"
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          >
+            <MenuItem primaryText="Increase" leftIcon={ <FontIcon className="fa fa-arrow-circle-up" /> } />
+            <MenuItem primaryText="Decrease" leftIcon={ <FontIcon className="fa fa-arrow-circle-down" /> } />
+            <Divider />
+            <MenuItem primaryText="Cancel" />
+          </IconMenu>
+          </CardHeader>
         </Card>
       )) }
     </div>
